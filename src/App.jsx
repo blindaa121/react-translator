7e34e103f2c253c18de781479e51175fe4284325
Bryan <blindaa121@gmail.com>
2020-10-08 15:13:07 -0700

    Implement languages component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import Field from './components/field';
+import Languages from './components/languages';
+import Translate from './components/translate';
+
+function App() {
+  const [language, setLanguage] = useState('ru');
+  const [text, setText] = useState('');
+
+  return (
+    <div className="App">
+      <Field label='Enter English' onChange={setText} value={text}/>
+      <Languages language={language} onLanguageChange={setLanguage}/>
+      <hr />
+      <Translate text={text} language={language}/>
+    </div>
+  );
+}
+
+export default App;
